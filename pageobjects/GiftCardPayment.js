ec5eae6d0765184aff4d5648eb5332df3f77d8d6
shridevimoole <shridevi.amashi@gmail.com>
2022-11-30 10:20:09 +1100

    pageobject

---
+++ pageobjects/GiftCardPayment.js
@@ -0,0 +1,12 @@
+const {test, expect} = require('@playwright/test');
+
+class GiftCardPayment
+{
+    constructor(page){
+        this.page = page; // making page to be used outside the constructor
+         // Storing locators in a variable
+         
+
+    }
+   
+}
